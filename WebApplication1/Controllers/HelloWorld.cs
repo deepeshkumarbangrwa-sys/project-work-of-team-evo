5357c7cb302e070a836e57fde2777df34ec04cc1
deepe <deepe@DEEPESH>
2025-11-26 15:34:37 +0000

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public string Index()
+        {
+            return "This is my first trial";
+        }
+        /*
+        public IActionResult Index()
+        {
+             
+           return View();
+        } 
+        */
+    }
+}
---
+++ WebApplication1/Models/ReportObject.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    
+public class ReportObject
+    {
+        public int PatientID { get; set; }
+        public int ReportID { get; set; } // PK when saved to DB
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double MaxPressure { get; set; }
+        public int TotalAlerts { get; set; }
+        public string SummaryText { get; set; }
+        public string FilePath { get; set; }
+    }
+}
